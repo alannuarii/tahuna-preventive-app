29ce8027bc3de4771c6ea8c4515c565996d04bfa
alannuarii <alannuariii@gmail.com>
2026-06-05 16:04:13 +0800

    refactor: remove download buttons and add security attributes to form links

--- pages/sop/[id].vue
+++ pages/sop/[id].vue
@@ -111,6 +111,7 @@
                   <a 
                     :href="form.path" 
                     target="_blank" 
+                    rel="noopener noreferrer"
                     class="formulir-view-link"
                   >
                     <div class="formulir-icon-wrapper">
@@ -122,21 +123,6 @@
                     <span class="formulir-text">
                       {{ form.title }}
                     </span>
-                  </a>
-
-                  <!-- Download Button -->
-                  <a 
-                    :href="form.path" 
-                    download 
-                    class="formulir-download-btn"
-                    title="Unduh Formulir"
-                    aria-label="Unduh Formulir"
-                  >
-                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-                      <polyline points="7 10 12 15 17 10"/>
-                      <line x1="12" y1="15" x2="12" y2="3"/>
-                    </svg>
                   </a>
                 </div>
               </div>
